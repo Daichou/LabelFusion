cb2d5734afc37ac3100b726156f73c58020b85ab
Ian Chen <ianre657@gmail.com>
2018-07-04 22:13:27 +0800

    use vtk6+qt5

--- docker/install_dependencies.sh
+++ docker/install_dependencies.sh
@@ -7,9 +7,15 @@ set -ex
 
 echo 'deb http://realsense-hw-public.s3.amazonaws.com/Debian/apt-repo xenial main' | sudo tee /etc/apt/sources.list.d/realsense-public.list
 
-sudo apt-key adv --keyserver keys.gnupg.net --recv-key 6F3EFCDE
+sudo apt-key adv --keyserver hkp://keys.gnupg.net:80 --recv-key 6F3EFCDE
 
 apt-get update
+
+# Removed packages
+#libqt4-dev \
+#libvtk5-dev \
+#libvtk5-qt4-dev \
+#python-vtk \
 
 apt-get install -y \
   bash-completion \
@@ -24,13 +30,20 @@ apt-get install -y \
   libopencv-dev \
   libopenni2-dev \
   libqhull-dev \
-  libqt4-dev \
+  qtbase5-gles-dev\
   libqwt-dev \
   libsuitesparse-dev \
   libudev-dev \
   libusb-1.0-0-dev \
-  libvtk5-dev \
-  libvtk5-qt4-dev \
+  libvtk6-dev \
+  libvtk6-java \
+  tcl-vtk6 \
+  libqt5webkit5 \
+  libqt5webkit5-dev \
+  libvtk6.2-qt \
+  libvtk6-qt-dev \
+  qtdeclarative5-dev \
+  libqt5quicktest5 \
   mesa-utils \
   openjdk-8-jdk \
   zlib1g-dev \
@@ -40,13 +53,12 @@ apt-get install -y \
   python-numpy \
   python-pip \
   python-scipy \
-  python-vtk \
+  python-vtk6 \
   python-yaml \
   librealsense2-dkms \
   librealsense2-utils \
   librealsense2-dev \
-  librealsense2-dbg \
-  sudo
+  librealsense2-dbg
 
 
   # optional cleanup to make the docker image smaller
